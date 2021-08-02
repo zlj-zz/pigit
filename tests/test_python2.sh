fb74ca187362cf233c7f8246cbed70fe100812cd
Zachary <zlj19971222@outlook.com>
2021-08-02 14:14:47 +0800

    update test for python2.

--- tests/test_python2.sh
+++ tests/test_python2.sh
@@ -8,22 +8,26 @@ Project=pygittools
 
 ${PY2} ${Project}/__init__.py
 Sleep 2
+${PY2} ${Project}/__init__.py -h
+Sleep 2
+${PY2} ${Project}/__init__.py -v
+Sleep 2
 ${PY2} ${Project}/__init__.py -s
 Sleep 2
 ${PY2} ${Project}/__init__.py -t
 Sleep 2
 ${PY2} ${Project}/__init__.py -S Branch
 Sleep 2
-${PY2} ${Project}/__init__.py -h
-Sleep 2
 ${PY2} ${Project}/__init__.py -f
 Sleep 2
 ${PY2} ${Project}/__init__.py -i
-Sleep 2
-${PY2} ${Project}/__init__.py -v
 Sleep 2
 ${PY2} ${Project}/__init__.py ws
 Sleep 2
 ${PY2} ${Project}/__init__.py wS
 Sleep 2
 ${PY2} ${Project}/__init__.py --count
+Sleep 2
+${PY2} ${Project}/__init__.py --create-ignore python
+cat ./.gitignore
+git checkout -- .gitignore
